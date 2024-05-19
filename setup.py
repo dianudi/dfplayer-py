80143eb67435d7835501ac84dfeec3e6931c943e
dianudi <144350056+dianudi@users.noreply.github.com>
2024-05-19 21:30:50 +0700

    fix & update readme

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='DFPlayer',
     packages=find_packages(exclude=['tests', 'assets']),
-    version='0.1.0',
+    version='0.1.2',
     description='Python3 library for the DFPlayer-mini',
     author='Dianudi',
     url="https://github.com/dianudi/dfplayer-py",
